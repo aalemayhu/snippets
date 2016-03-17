d3bbd4670cd3d15e9caf0d4e85134c05abc77b71
Alexander Alemayhu <alexander@alemayhu.com>
2016-03-17 14:55:34 +0100

    using dropFirst, let the swift stdlib do the slicing

--- swift/file-name-namer.swift
+++ swift/file-name-namer.swift
@@ -1,17 +1,14 @@
 import Foundation
 
 func run() {
-    let args = Process.arguments
-
-    guard args.count > 1 else {
+    let args = Process.arguments.dropFirst()
+    guard args.count > 0 else {
         print("Please supply strings to be used for the name")
         return
     }
-
 		// The first element is the application name, which we don't care about in
 		// this case.
-    let slice = args[1..<args.count]
-    let joined = slice.joinWithSeparator("-")
+    let joined = args.joinWithSeparator("-")
     print (joined.lowercaseString)
 }
 
